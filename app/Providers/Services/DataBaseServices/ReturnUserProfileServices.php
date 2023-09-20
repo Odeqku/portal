d225f3f88bb00bb9549c4e0042b1fff64dcd826f
Abdulganiyu <113687858+Odeqku@users.noreply.github.com>
2023-09-20 16:57:10 +0100

    Successfully deployed 2nd layer of Permissions and  Roles in this project Portal

--- app/Providers/Services/DataBaseServices/ReturnUserProfileServices.php
+++ app/Providers/Services/DataBaseServices/ReturnUserProfileServices.php
@@ -1,25 +1,23 @@
 <?php
 
 namespace App\Providers\Services\DataBaseServices;
-use App\Providers\Services\DataBaseServices\NewUserService;
+use App\Providers\Services\DataBaseServices\ReturnAllUsersServices;
 
-class ReturnUserProfileServices
-{
-    
+class ReturnUserProfileServices extends ReturnAllUsersServices
+{    
     public function userProfile()
     {   
-        // dd('UserProfile');
-        return app('users_service')->authUser()->profile->profileable_type;
-        // if($user->authUser()->hasRole('Student')){
-        //     return 'Student';
-        // }
+        
+        if($this->authUser()->hasRole('Student')){
+            return 'Student';
+        }
 
-        // if($user->authUser()->hasRole('Admin')){
-        //     return 'Admin';
-        // }
+        if($this->authUser()->hasRole('Admin')){
+            return 'Admin';
+        }
 
-        // if($user->authUser()->hasRole('super admin')){
-        //     return 'super admin';
-        // }
+        if($this->authUser()->hasRole('super admin')){
+            return 'super admin';
+        }
     }
 }
